b3af5a263ce5f60becf552ec850d1dcefddb5bc5
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-04-14 09:16:05 -0300

    Updates

---
+++ precache-manifest.b12653bb051146680aab50ae00a42486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4454383494a2f55626237bfd4dffb8",
+    "url": "/react-netflix-clone/index.html"
+  },
+  {
+    "revision": "f0bf59b155a3f8e86216",
+    "url": "/react-netflix-clone/static/css/main.71b1dd27.chunk.css"
+  },
+  {
+    "revision": "ad52929dd28b571d9764",
+    "url": "/react-netflix-clone/static/js/2.cc0b2a5f.chunk.js"
+  },
+  {
+    "revision": "d79b9d5b8d60d47da305228bd4bef3db",
+    "url": "/react-netflix-clone/static/js/2.cc0b2a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bf59b155a3f8e86216",
+    "url": "/react-netflix-clone/static/js/main.814379de.chunk.js"
+  },
+  {
+    "revision": "bd6bcde01801f483b937",
+    "url": "/react-netflix-clone/static/js/runtime-main.cd0db945.js"
+  }
+]);
